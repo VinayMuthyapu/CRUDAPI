8bc6e47242b6eb9c1798794dbc5b2cfd98111bc9
VinayMuthyapu <VinayMuthyapu@users.noreply.github.com>
2018-02-16 12:26:52 +0530

    Added Add column

---
+++ DynamicTable.js
@@ -0,0 +1,79 @@
+$(document).ready(function()
+		{
+			
+			//$("input").prop('required',true);
+			$("input[value='Add Row']").click(function(event)
+			{
+						event.preventDefault();
+						var name=$("#name").val();
+						var email=$("#email").val();
+						if(name && email)
+						{
+							var markup = "<tr class = 'tblRow'><td>"+name+"</td><td>"+email+"</td></tr>";
+							$("table tbody").append(markup);
+							$('#name').val('');
+							$('#email').val('');
+						}
+						else
+						{
+							alert("Enter both fields");
+						}
+			});
+			$("input[id='Add-Column']").click(function(event)
+			{
+				var columnName=$("input[id='column-name").val();
+				var getRowCount=$('#table tr').length;
+				console.log(getRowCount);
+				if(columnName)
+				{
+					var mkp = "<th>"+columnName+"</th>";
+					$("table thead tr").append(mkp);
+					$("#column-name").val('');
+					console.log(getRowCount);
+				}
+				else
+				{
+					alert("Enter Column Name");
+				}
+				
+			});
+			$(".btn-primary").click(function()
+			{
+				/*$("table tbody").find('input[name = "record" ]').each(function()
+					{
+						if($(this).is(":checked"))
+						{
+							$(this).parents("tr").remove();
+						}
+					});*/
+					$('.highlight_row').remove();
+			});
+			$('#table').on('click', '.tblRow', function (event)
+			{	
+				//console.log(event.target);
+				//console.log("clicked");
+				
+				if (!$(event.target.closest('tr')).hasClass('highlight_row') ) {
+						$(this).closest('tr').addClass("highlight_row");
+				} else {
+					$(this).closest('tr').removeClass("highlight_row");
+				}
+				
+				/*if (event.target.type !== 'checkbox') 
+				{
+					$(':checkbox', this).trigger('click');
+				}*/
+			});
+
+			/*$("input[type='checkbox']").change(function (e)
+			{
+				if ($(this).is(":checked"))
+				{
+					$(this).closest('tr').addClass("highlight_row");
+				} 
+				else
+				{
+					$(this).closest('tr').removeClass("highlight_row");
+				}
+			});*/
+		});
